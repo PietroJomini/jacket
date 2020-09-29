0b2e3405a1c9c0b81091cdbc1c71f6a58508c67d
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-09-29 15:05:37 +0200

    modularization

---
+++ mod.ts
@@ -0,0 +1,10 @@
+export { Server } from "./src/server.ts";
+export { Router } from "./src/router.ts";
+export { types } from "./src/schema.ts";
+export { controller } from "./src/controller.ts";
+export { ChainHalt, ensure } from "./src/chain.ts";
+
+export type { Route, HttpMethod } from "./src/router.ts";
+export type { Infer } from "./src/schema.ts";
+export type { Controller } from "./src/controller.ts";
+export type { ChainMiddleware } from "./src/chain.ts";
