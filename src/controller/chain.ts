509a247ed802249daed5c464feac71fd0c47446b
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-09-29 14:34:58 +0200

    controller chain

---
+++ src/controller/chain.ts
@@ -0,0 +1,35 @@
+import type { Context } from "../../deps.ts";
+
+export type ChainMiddleware<Q> = (P: {
+  query: Q;
+  payload: any;
+  ctx: Context;
+}) => any | Promise<any>;
+
+export type ChainHaltPayload = {
+  msg?: string;
+  status?: number;
+};
+
+type ChainHaltP = {
+  msg?: string;
+  status?: number;
+};
+
+export class ChainHalt {
+  status: number;
+  msg: string;
+  constructor({ msg = "halt", status = 500 }: ChainHaltP = {}) {
+    this.msg = msg;
+    this.status = status;
+  }
+
+  body() {
+    return { msg: this.msg, status: this.status };
+  }
+}
+
+export const ensure = (
+  C: boolean,
+  P: ChainHaltP,
+) => !C ? new ChainHalt(P) : null;
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,31 +1,32 @@
 import type { Context, Middleware } from "../../deps.ts";
 import type { Infer } from "../schema.ts";
-
-export type ControllerCallback<Q> = (P: {
-  query: Q;
-  patload: any;
-  ctx: Context;
-}) => any | Promise<any>;
+import { ChainMiddleware, ChainHalt } from "./chain.ts";
 
 export type Controller<Q = any> = {
-  use: (callback: ControllerCallback<Q>) => Controller<Q>;
+  use: (callback: ChainMiddleware<Q>) => Controller<Q>;
   construct: () => Middleware;
 };
 
 export const controller = <S, Q = Infer<S>>(
   schema: S,
-  chain: ControllerCallback<Q>[] = [],
+  chain: ChainMiddleware<Q>[] = [],
 ): Controller<Q> => ({
-  use: (C: ControllerCallback<Q>) => controller(schema, [...chain, C]),
+  use: (C: ChainMiddleware<Q>) => controller(schema, [...chain, C]),
   construct: () => {
     const validator = (schema as any).destruct();
 
     return async (ctx: Context) => {
       const [err, query] = validator(ctx.state.body);
-      if (err) return (ctx.response.body = err);
+      if (err) return ctx.response.body = err;
 
-      console.log(chain);
-      ctx.response.body = query;
+      let payload: any;
+      for await (const middleware of chain) {
+        const MP = await middleware({ query, payload, ctx });
+        if (MP instanceof ChainHalt) return ctx.response.body = MP.body();
+        if (MP) payload = MP;
+      }
+
+      if (!ctx.response.body) ctx.response.body = payload;
     };
   },
 });
