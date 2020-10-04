f910d23579ebfc3cfb6e33b060961aa036e28011
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-10-04 15:24:02 +0200

    boolean type allow test of Boolean values

--- src/schema/types/boolean.ts
+++ src/schema/types/boolean.ts
@@ -2,6 +2,7 @@ import { Validator } from "../validator.ts";
 import { ValidationError } from "../Errors.ts";
 
 export const boolean = new Validator((V) => {
+  if (typeof V === "boolean") return V;
   if (V === "true") return true;
   if (V === "false") return false;
   throw new ValidationError("boolean");
